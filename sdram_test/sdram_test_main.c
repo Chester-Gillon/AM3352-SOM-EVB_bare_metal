d818c3fec2727966ece844367b301a62fb6d8e32
Chester Gillon <chester.gillon@metronet.co.uk>
2015-04-03 21:34:15 +0100

    For sdram_test enable MMU as well as the cache, to make memory cachable

--- sdram_test/sdram_test_main.c
+++ sdram_test/sdram_test_main.c
@@ -16,6 +16,7 @@
 #include <consoleUtils.h>
 #include <rtc.h>
 #include <cache.h>
+#include <mmu.h>
 
 #define SDRAM_SIZE_BYTES (512 * 1024 * 1024)
 #define SDRAM_SIZE_WORDS (SDRAM_SIZE_BYTES / sizeof (uint32_t))
@@ -28,6 +29,73 @@
 #define HOUR_SHIFT           (24)
 #define MINUTE_SHIFT         (16)
 #define SECOND_SHIFT         (8)
+
+#define START_ADDR_DDR             (0x80000000)
+#define START_ADDR_DEV             (0x44000000)
+#define START_ADDR_OCMC            (0x40300000)
+#define NUM_SECTIONS_DDR           (512)
+#define NUM_SECTIONS_DEV           (960)
+#define NUM_SECTIONS_OCMC          (1)
+
+/* page tables start must be aligned in 16K boundary */
+#pragma DATA_ALIGN(pageTable, 16384);
+static volatile unsigned int pageTable[4*1024];
+
+/*
+** Function to setup MMU. This function Maps three regions (1. DDR
+** 2. OCMC and 3. Device memory) and enables MMU.
+*/
+static void MMUConfigAndEnable(void)
+{
+    /*
+    ** Define DDR memory region of AM335x. DDR can be configured as Normal
+    ** memory with R/W access in user/privileged modes. The cache attributes
+    ** specified here are,
+    ** Inner - Write through, No Write Allocate
+    ** Outer - Write Back, Write Allocate
+    */
+    REGION regionDdr = {
+                        MMU_PGTYPE_SECTION, START_ADDR_DDR, NUM_SECTIONS_DDR,
+                        MMU_MEMTYPE_NORMAL_NON_SHAREABLE(MMU_CACHE_WT_NOWA,
+                                                         MMU_CACHE_WB_WA),
+                        MMU_REGION_NON_SECURE, MMU_AP_PRV_RW_USR_RW,
+                        (unsigned int*)pageTable
+                       };
+    /*
+    ** Define OCMC RAM region of AM335x. Same Attributes of DDR region given.
+    */
+    REGION regionOcmc = {
+                         MMU_PGTYPE_SECTION, START_ADDR_OCMC, NUM_SECTIONS_OCMC,
+                         MMU_MEMTYPE_NORMAL_NON_SHAREABLE(MMU_CACHE_WT_NOWA,
+                                                          MMU_CACHE_WB_WA),
+                         MMU_REGION_NON_SECURE, MMU_AP_PRV_RW_USR_RW,
+                         (unsigned int*)pageTable
+                        };
+
+    /*
+    ** Define Device Memory Region. The region between OCMC and DDR is
+    ** configured as device memory, with R/W access in user/privileged modes.
+    ** Also, the region is marked 'Execute Never'.
+    */
+    REGION regionDev = {
+                        MMU_PGTYPE_SECTION, START_ADDR_DEV, NUM_SECTIONS_DEV,
+                        MMU_MEMTYPE_DEVICE_SHAREABLE,
+                        MMU_REGION_NON_SECURE,
+                        MMU_AP_PRV_RW_USR_RW  | MMU_SECTION_EXEC_NEVER,
+                        (unsigned int*)pageTable
+                       };
+
+    /* Initialize the page table and MMU */
+    MMUInit((unsigned int*)pageTable);
+
+    /* Map the defined regions */
+    MMUMemRegionMap(&regionDdr);
+    MMUMemRegionMap(&regionOcmc);
+    MMUMemRegionMap(&regionDev);
+
+    /* Now Safe to enable MMU */
+    MMUEnable((unsigned int*)pageTable);
+}
 
 /**
  * @brief This function is used to initialize and configure UART Module.
@@ -137,6 +205,7 @@ int main (void)
     uint32_t index;
     uint32_t num_errors;
 
+    MMUConfigAndEnable ();
     CacheEnable (CACHE_ALL);
     UART_setup ();
     RTC_setup ();
