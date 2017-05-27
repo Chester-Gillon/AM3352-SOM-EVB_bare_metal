b99ab552f96f4cab27094f9e5937e012725eee90
Chester Gillon <chester.gillon@metronet.co.uk>
2017-05-27 16:35:46 +0100

    Update to use CCS 7.1 for Windows, to match Linux

--- setup_cmake.bat
+++ setup_cmake.bat
@@ -1,10 +1,11 @@
-rem Set up the cmake build environment under Linux, to a clean state
+rem Set up the cmake build environment under Windows, to a clean state
 
-set CCS_INSTALL_ROOT=c:/ti_ccs6_1
-set ARM_GCC_ROOT=%CCS_INSTALL_ROOT%/ccsv6/tools/compiler/gcc-arm-none-eabi-4_8-2014q3
-set CG_TOOL_ROOT=%CCS_INSTALL_ROOT%/ccsv6/tools/compiler/ti-cgt-arm_5.2.7
-set CMAKE_MAKE_PROGRAM=%CCS_INSTALL_ROOT%/ccsv6/utils/bin/gmake.exe
-set STARTERWARE_ROOT=%CCS_INSTALL_ROOT%/AM335X_StarterWare_02_00_01_01
+set TI_INSTALL_ROOT=c:/ti
+set CCS_INSTALL_ROOT=%TI_INSTALL_ROOT%/ccs710
+set ARM_GCC_ROOT=%CCS_INSTALL_ROOT%/ccsv7/tools/compiler/gcc-arm-none-eabi-4_9-2015q3
+set CG_TOOL_ROOT=%CCS_INSTALL_ROOT%/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS
+set CMAKE_MAKE_PROGRAM=%CCS_INSTALL_ROOT%/ccsv7/utils/bin/gmake.exe
+set STARTERWARE_ROOT=%TI_INSTALL_ROOT%/AM335X_StarterWare_02_00_01_01
 
 rmdir /s /q ..\Release
 rmdir /s /q ..\Debug
