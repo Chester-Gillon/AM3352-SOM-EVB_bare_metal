7342f81e6bac2a6df853c52ffda8d6b9ff2dcbbb
Chester Gillon <chester.gillon@metronet.co.uk>
2018-05-06 16:01:13 +0100

    Update to use CCS 8.0 and GCC 7.2.1, to match Linux

--- setup_cmake.bat
+++ setup_cmake.bat
@@ -1,10 +1,11 @@
 rem Set up the cmake build environment under Windows, to a clean state
 
 set TI_INSTALL_ROOT=c:/ti
-set CCS_INSTALL_ROOT=%TI_INSTALL_ROOT%/ccs730
-set ARM_GCC_ROOT=%CCS_INSTALL_ROOT%/ccsv7/tools/compiler/gcc-arm-none-eabi-6-2017-q1-update
-set CG_TOOL_ROOT=%CCS_INSTALL_ROOT%/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS
-set CMAKE_MAKE_PROGRAM=%CCS_INSTALL_ROOT%/ccsv7/utils/bin/gmake.exe
+set CCS_INSTALL_ROOT=%TI_INSTALL_ROOT%/ccs800
+set ECLIPSE_EXE="%CCS_INSTALL_ROOT%/ccsv8/eclipse/eclipse"
+set ARM_GCC_ROOT=%CCS_INSTALL_ROOT%/ccsv8/tools/compiler/gcc-arm-none-eabi-7-2017-q4-major-win32
+set CG_TOOL_ROOT=%CCS_INSTALL_ROOT%/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS
+set CMAKE_MAKE_PROGRAM=%CCS_INSTALL_ROOT%/ccsv8/utils/bin/gmake.exe
 set STARTERWARE_ROOT=%TI_INSTALL_ROOT%/AM335X_StarterWare_02_00_01_01
 
 rmdir /s /q ..\Release
@@ -14,9 +15,9 @@ mkdir ..\Release || goto :error
 mkdir ..\Debug || goto :error
 
 cd ../Release
-cmake -G "Eclipse CDT4 - Unix Makefiles" -DPLATFORM_TYPE=Release -DCCS_INSTALL_ROOT="%CCS_INSTALL_ROOT%" -DARM_GCC_ROOT="%ARM_GCC_ROOT%" -DSTARTERWARE_ROOT="%STARTERWARE_ROOT%" -DCG_TOOL_ROOT="%CG_TOOL_ROOT%" -DCMAKE_MAKE_PROGRAM="%CMAKE_MAKE_PROGRAM%" ..\src
+cmake -G "Eclipse CDT4 - Unix Makefiles" -DPLATFORM_TYPE=Release -DCCS_INSTALL_ROOT="%CCS_INSTALL_ROOT%" -DARM_GCC_ROOT="%ARM_GCC_ROOT%" -DSTARTERWARE_ROOT="%STARTERWARE_ROOT%" -DCG_TOOL_ROOT="%CG_TOOL_ROOT%" -DCMAKE_MAKE_PROGRAM="%CMAKE_MAKE_PROGRAM%" -DCMAKE_ECLIPSE_EXECUTABLE=%ECLIPSE_EXE% ..\src
 cd ../Debug
-cmake -G "Eclipse CDT4 - Unix Makefiles" -DPLATFORM_TYPE=Debug -DCCS_INSTALL_ROOT="%CCS_INSTALL_ROOT%" -DARM_GCC_ROOT="%ARM_GCC_ROOT%" -DSTARTERWARE_ROOT="%STARTERWARE_ROOT%" -DCG_TOOL_ROOT="%CG_TOOL_ROOT%" -DCMAKE_MAKE_PROGRAM="%CMAKE_MAKE_PROGRAM%" ..\src
+cmake -G "Eclipse CDT4 - Unix Makefiles" -DPLATFORM_TYPE=Debug -DCCS_INSTALL_ROOT="%CCS_INSTALL_ROOT%" -DARM_GCC_ROOT="%ARM_GCC_ROOT%" -DSTARTERWARE_ROOT="%STARTERWARE_ROOT%" -DCG_TOOL_ROOT="%CG_TOOL_ROOT%" -DCMAKE_MAKE_PROGRAM="%CMAKE_MAKE_PROGRAM%" -DCMAKE_ECLIPSE_EXECUTABLE=%ECLIPSE_EXE% ..\src
 
 cd ../src
 goto :end
